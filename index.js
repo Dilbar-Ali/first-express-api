e6298c82674e9a3a0b2f40c71b359166326987e6
Dilbar-ali <dilbarali0013@gmail.com>
2022-11-09 19:54:43 +0500

    Express APi

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app=express();
 const port=8080;
 // app.use('/api/v1')
 
+app.get("/pages", async(req,res)=>{
+    try{
+        res.send("<h1>HEllow Developers </h1>")
+        console.log("Hellow Develpers ");
+
+    }
+    catch(error){
+      
+        console.log("The error is occure is ", error)
+    }
+})
 
 app.listen(port,()=>{
     console.log("App is running on port ", port);
